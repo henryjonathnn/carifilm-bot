ac5ff3784ee08567f50539e8a3e1640c0d90f5f8
henryjonathnn <henryjonathanchandra12@gmail.com>
2025-02-03 15:53:42 +0700

    create and connect the bot by token

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api')
 
-const token = '' // masih kosong
+const token = '7882871404:AAHXMAw6SxL1aT1ZzIAKqsUGcjgsRxj3Vvw'
 
 const bot = new TelegramBot(token, { polling: true })
 
 console.log('Bot is running...')
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id
+    bot.sendMessage(chatId, "Bot berhasil jalan! ✅")
+})
+
+bot.on('polling_error', (error) => {
+    console.error('Error:', error);
+});
